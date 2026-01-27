3138e395e446f036dcc1f29edcb3384a5f251b3f
CoolLKK PS <PSKyle5926@outlook.com>
2026-01-28 06:33:08 +0800

    2.4.8

--- PizzaTowerEscapeMusic/Properties/AssemblyInfo.cs
+++ PizzaTowerEscapeMusic/Properties/AssemblyInfo.cs
@@ -1,11 +1,11 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("2.4.7.0")]
+[assembly: AssemblyVersion("2.4.8.0")]
 [assembly: AssemblyCompany("PizzaTowerEscapeMusic")]
 [assembly: AssemblyConfiguration("Release")]
 [assembly: AssemblyDescription("Plays music from Pizza Tower when the early ship leave alert appears")]
-[assembly: AssemblyFileVersion("2.4.7.0")]
-[assembly: AssemblyInformationalVersion("2.4.7")]
+[assembly: AssemblyFileVersion("2.4.8.0")]
+[assembly: AssemblyInformationalVersion("2.4.8")]
 [assembly: AssemblyProduct("PizzaTowerEscapeMusic")]
 [assembly: AssemblyTitle("PizzaTowerEscapeMusic")]
 // [assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
